2a8e3a885eb58ec7be64e7e72c35ace1dd79a834
Jean-Baptiste Lugagne <jb.lugagne@gmail.com>
2016-03-10 11:51:30 +0100

    working version

--- autofocus.m
+++ autofocus.m
@@ -24,47 +24,64 @@ function autofocus()
         % If it has, acquire sharpness curve: 
         initZ = mmc.getPosition('PIZStage');
         pause(.1) % What's this?
-        Range = cumsum(repmat(AS.CoarseSS,1,(round(.5*AS.CoarseR/AS.CoarseSS))));
-        Range = initZ + [fliplr(-Range) 0 Range];
+        
+        % Calculate the range:
+        nbsteps = round(.5*AS.CoarseR/AS.CoarseSS);
+        switch AS.method
+            case 'linear'
+                Range = cumsum(repmat(AS.CoarseSS,1,nbsteps));
+                Range = initZ + [fliplr(-Range) 0 Range];
+            case 'log'
+                Range = logspace(log10(6*AS.CoarseSS/nbsteps),log10(AS.CoarseR),nbsteps);
+                Range = initZ + [fliplr(-Range) 0 Range];
+            otherwise
+                error(['Unknown range method: ' AS.method])
+        end
+        
+        % Acquisition:
         [AS.Z_meas, AS.Sharpness_meas] = Zstack_sharpness(Range,AS.box);
 
         
         % The function we will have to minimize:
-        funOPT = @(s) AS.reffunction(AS.Z_meas+s(1)) ...  S(1) = shift in z
-            - AS.Sharpness_meas.*s(2) ... % s(2) = scale
-            + s(3); % S(3) = overall shift in sharpness
-        params = lsqnonlin(funOPT,[0,1,0],[-30,.3,-max(AS.Sharpness_truth)/10],[100,1.5,max(AS.Sharpness_truth)/10]);
+        funOPT = @(s) AS.reffunction(AS.Z_meas-s(1))' ...  S(1) = shift in z
+            - (AS.Sharpness_meas.*s(2) ... % s(2) = scale
+            + s(3)); % S(3) = overall shift in sharpness
         
-        
-        % The cross-correlation mthod:
-        meas = AS.Sharpness_meas;
-        for ind1 = 1:(numel(AS.Sharpness_truth)-AS.setupratio*numel(meas))
-            c(ind1) = sum((meas-AS.Sharpness_truth(ind1:AS.setupratio:AS.setupratio*(numel(meas)-1)+ind1)).^2);
+        % I first get a rough estimate of where the minimum might be:
+        % (extremely inefficient but at least it works)
+        c= [];
+        bs = -50:.1:50;
+        if isempty(AS.scaletime), scale = 1; Sshift = 0; 
+        else scale = AS.scaletime(end); Sshift = AS.Sharpshifttime(end);
         end
-        % Find max of Xcorrelation and compute position of user-defined
-        % focus
-        [~,imax] = min(c);
-        truthoffset = AS.Z_meas(1) - AS.Z_truth(imax);
-        AS.NewZ = AS.init_Z+truthoffset;
+            
+        for ind1 = bs
+            c(end+1) = sum(funOPT([ind1,scale,Sshift]).^2);
+        end
+        [~,i] = min(c);
+        
+        
+        % And then I run the optimization algorithm with this acquisition as the starting point for the optimization:
+        [params, resnorm, ~, ~, ~] = lsqnonlin(funOPT,[bs(i),1,0],[bs(1),.3,-max(AS.Sharpness_truth)/10],[bs(end),1.5,max(AS.Sharpness_truth)/10]);
+        
+        AS.NewZ = AS.init_Z+params(1);
+        
         
         
         % Update the time vectors:
         AS.exectimes(end+1) = toc(AS.starttic);
         AS.measZtime(end+1) = AS.NewZ;
-        AS.errortime(end+1) = AS.NewZ - initZ;
-        AS.funoffset(end+1) = params(1);
-        AS.Xoffset(end+1) = truthoffset;
-        if numel(AS.exectimes) > 1
-            smootherror = smooth(AS.exectimes, AS.errortime, 1, 'rloess');
-            AS.setZtime(end+1) = smootherror(end) + initZ;
-        else
-            AS.setZtime(end+1) = AS.NewZ;
-        end
+        AS.shifttime(end+1) = params(1);
+        AS.scaletime(end+1) = params(2);
+        AS.Sharpshifttime(end+1) = params(3);
+        AS.resnormtime(end+1) = resnorm;
+        
+        % And finally I run the smoothing algorithm:
+        AS.decisiontime(end+1) = AS.init_Z + smoothlastNwithrejection(AS.shifttime,5,2);
         
         
         % Let's see those results:
-        disp(['[Autofocus] Function minimization: Z Shift = ' num2str(params(1)) ', Scaling = ' num2str(params(2)) ', Sharpness shift = ' num2str(params(3)) ])
-        disp(['[Autofocus] Points cross correlation: Z shift = ' num2str(truthoffset) ])
+        disp(['[Autofocus] Function minimization: Z Shift = ' num2str(params(1)) ', Scaling = ' num2str(params(2)) ', Sharpness shift = ' num2str(params(3)) ', Normalized residual = ' num2str(resnorm) ])
         
         
         % Plot curve comparison:
@@ -76,12 +93,15 @@ function autofocus()
         end
         cla
         title('Autofocus')
-        plot(AS.Z_truth,AS.Sharpness_truth,'m--');
         hold on;
-        plot(AS.Z_meas,AS.Sharpness_meas,'bx');
-        plot(AS.Z_truth+truthoffset,AS.Sharpness_truth,'m');
-        line([AS.NewZ AS.NewZ],[ylim],'Color','g');
-        line([initZ initZ],[ylim],'Color','r');
+        plot(AS.Z_meas,AS.Sharpness_meas.*params(2)+params(3),'bx');
+        plot(AS.Z_truth+params(1),AS.reffunction(AS.Z_truth),'r');
+        line([AS.NewZ AS.NewZ],[ylim],'Color','g','LineStyle','-');
+        line([initZ initZ],[ylim],'Color','k','LineStyle','--');
+        xlabel('Z position (um)')
+        ylabel('Sharpness (a.u.)')
+        xlim([-4 84]);
+        
         
         
         % Plot the results over time:
@@ -94,8 +114,8 @@ function autofocus()
         cla
         hold on
         title('Focus over time')
-        plot(AS.exectimes./3600,AS.Xoffset,'r.');
-        plot(AS.exectimes./3600,AS.funoffset,'bx');
+        plot(AS.exectimes./3600,AS.measZtime,'r.');
+        plot(AS.exectimes./3600,AS.decisiontime,'b');
         xlabel('time (h)')
         ylabel('position (um)')
         
@@ -124,18 +144,19 @@ persistent default_settings
 
 
 if isempty(default_settings) % If it is not empty, we do nothing, as it is a persistent variable.
-    default_settings = {'1', '20', '150'};
+    default_settings = {'1', '20', '150', 'log'};
 end
 % Launch preview window
 mmGUI.enableLiveMode(true);
 
 % Store initial Zs
 % User sets focus with wheel, modifies settings if necessary and clicks on button when done
-x = inputdlg({'step size','range (X microns)','size of ROI'},'Autofocus settings', 1, default_settings);
+x = inputdlg({'step size','range (X microns)','size of ROI','method (linear or log)'},'Autofocus settings', 1, default_settings);
 default_settings = x;
 CoarseSS = str2double(x{1}); % Step size
 CoarseR = str2double(x{2}); % Range
 refS = str2num(x{3});
+method = x{4};
 SetupSS = CoarseSS/AS.setupratio;
 
 mmGUI.enableLiveMode(false);
@@ -184,16 +205,32 @@ py = pos(2)+refS/2;
 AS.ref = img((px-refS/2):(px + refS/2), (py-refS/2):(py+refS/2));
 AS.init_Sharpness = estimate_sharpness(double(AS.ref));
 
-% go +-range*1.5 en finestepsize
-Range = cumsum(repmat(SetupSS,1,(round((CoarseR/SetupSS)*1.5))));
-Range = AS.init_Z + [fliplr(-Range) 0 Range];
+% Calculate the range:
+nbsteps = (round((CoarseR/SetupSS)*1.5));
+switch method
+    case 'linear'
+        Range = cumsum(repmat(SetupSS,1,(round((CoarseR/SetupSS)*1.5))));
+        Range = AS.init_Z + [fliplr(-Range) 0 Range];
+    case 'log'
+        Range = logspace(log10(6*CoarseSS/nbsteps),log10(CoarseR),nbsteps);
+        Range = AS.init_Z + [fliplr(-Range) 0 Range];
+    otherwise
+            error(['Unknown range method: ' method])
+end
+
 [AS.Z_truth, AS.Sharpness_truth] = Zstack_sharpness(Range,AS.box);
+%  if there are duplicate valuies, get rid of them
+[~, alluniind] = unique(AS.Z_truth);
+duplicate_ind = setdiff(1:size(AS.Z_truth), alluniind);
+AS.Z_truth = AS.Z_truth(setdiff(1:numel(AS.Z_truth), duplicate_ind));
+AS.Sharpness_truth = AS.Sharpness_truth(setdiff(1:numel(AS.Sharpness_truth), duplicate_ind));
 
 % return to initial Z
 mmc.setPosition('PIZStage',AS.init_Z );
 
 % Get a spline function estimate of the points:
-AS.reffunction = fit(AS.Z_truth',AS.Sharpness_truth','spline');
+AS.reffunction = fit(AS.Z_truth,AS.Sharpness_truth','smoothingspline');
+
 
 % Show the sharpness curve
 fa = findobj('type','figure','name','autofocus');
@@ -206,21 +243,23 @@ cla
 title('Autofocus')
 plot(AS.reffunction,AS.Z_truth, AS.Sharpness_truth);
 hold on
-line([AS.initZ AS.initZ],[ylim],'Color','g');
-
-
+line([AS.init_Z AS.init_Z],[ylim],'Color','g');
 
 
 AS.CoarseSS = CoarseSS;
 AS.CoarseR = CoarseR;
-AS.initialized = true;
+AS.method = method;
 AS.measZtime = [];
-AS.setZtime = [];
 AS.errortime = [];
 AS.exectimes = [];
 AS.starttic = tic();
-AS.funoffset = [];
-AS.Xoffset = [];
+AS.shifttime = [];
+AS.scaletime = [];
+AS.Sharpshifttime = [];
+AS.resnormtime = [];
+AS.decisiontime = [];
+
+AS.initialized = true;
 end
 
 % Z Acquisition function:
@@ -259,4 +298,35 @@ mmc.setProperty('Light shutter (Vincent-D1)','Command','Close');
 mmc.setAutoShutter(true);
 mmc.setROI(oldROI.getX(),oldROI.getY(),oldROI.getWidth(),oldROI.getHeight());
 mmc.setProperty('Camera-1','ClearMode','Clear Pre-Exposure')
+end
+
+
+% Utilisties
+function output = smoothlastNwithrejection(input, span, thresholdinsigmas)
+
+    if numel(input) < span
+        span = numel(input);
+    end
+
+    smoothedin = mysmooth(input,span);
+    sigma = std(input'-smoothedin); % Weird I know
+    input(abs(input'-smoothedin) > thresholdinsigmas*sigma) = input(abs(input'-smoothedin) > thresholdinsigmas*sigma).*NaN;
+    lastN = input((end+1-span):end);
+    lastN = lastN(~isnan(lastN));
+    output = mean(lastN);
+
+    if isempty(output) || isnan(output)
+         output = smoothedin(end);
+
+    end
+end
+
+function output = mysmooth(input, span)
+
+    output = smooth(input,span);
+
+    for ind1 = 1:floor(span/2)
+        output(ind1) = mean(input(1:(floor(span/2)+ind1-1)));
+        output(end+1-ind1) = mean(input((end+1-(floor(span/2)+ind1)):end));
+    end
 end
